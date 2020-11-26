d870e92523c116f355bcde301dd110ca855e6c66
JhoanGarcia <jhoan.garcia@avaldigitallabs.com>
2020-11-26 17:31:34 -0500

    presentacion

--- cypress/integration/DemoADL/demoBlaze.js
+++ cypress/integration/DemoADL/demoBlaze.js
@@ -1,20 +1,20 @@
 describe('HU: Consumo de Apis segun categoría', function(){    
 
     it('Nombre Escenario1', function(){
-        cy.server()
-        cy.route('POST', '**/view').as('postCart')
+      cy.server()
+      //cy.route('POST', '**/view').as('view')
 
-        cy.getMocksError(200)
-        cy.visit('https://www.demoblaze.com/')
-        cy.contains('Laptops').click()
-      //  cy.getMocksView(200)
-        cy.get(':nth-child(1) > .card > :nth-child(1) > .card-img-top').click()
-        cy.get('.price-container').should('contain', '790')
 
-        cy.get('.col-sm-12 > .btn').click()
-        //cy.getMocksView(200)
-        cy.get('#cartur').click()
-        cy.wait('@postCart').its('responseBody').should('have.property', 'title', 'Sony vaio i10')
+      cy.visit('https://www.demoblaze.com/')
+
+      cy.get('.card-title').first().click()
+      cy.get('.col-sm-12 > .btn').click()
+      //cy.get('.name').should('be.visible').and('have.text','Samsung galaxy s6')
+     cy.getMocksView(200)     
+      cy.get('#cartur').click()
+     // cy.get('.success > :nth-child(2)').should('be.visible').and('have.text','Samsung galaxy chapter ADL')
+      //cy.wait('@view').its('responseBody').should('have.property', 'id', 1)
+
     })
 
 })
